31c813c6e7dd2a1f74014e58cdd70b2e81b91c8a
Sistely Nazareth <sistely.nazareth@hotmail.com>
2021-12-29 03:04:16 -0300

    TP04 Xamarin

---
+++ TP04/TP04.iOS/SQLite_IOS.cs
@@ -0,0 +1,32 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TP04.Data;
+using TP04.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+
+namespace TP04.iOS
+{
+    public class SQLite_iOS : ISQLite
+    {
+        public SQLite_iOS()
+        {
+        }
+        public SQLite.SQLiteConnection GetConexao()
+        {
+            var arquivodb = "ifspdb.db3";
+            string caminho = Environment.GetFolderPath(
+            Environment.SpecialFolder.Personal);
+            string bibliotecaPessoal = Path.Combine(caminho, "..", "Library");
+            var local = Path.Combine(bibliotecaPessoal, arquivodb);
+            var conexao = new SQLite.SQLiteConnection(local);
+            return conexao;
+        }
+    }
+}
